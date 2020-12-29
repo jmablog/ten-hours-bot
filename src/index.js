3105438a09a6b9a02001a16782787c0e2e196e45
Sophie Koonin <sophie@localghost.dev>
2020-12-29 18:15:59 +0000

    Get video titles from youtube

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const Twitter = require('twitter-lite');
 require('dotenv').config();
+const { google, gamesConfiguration_v1configuration } = require('googleapis');
+
+const youtube = google.youtube({
+  auth: process.env.GOOGLE_API_KEY,
+  version: 'v3',
+});
 
 const client = new Twitter({
   consumer_key: process.env.CONSUMER_KEY,
@@ -10,7 +16,19 @@ const client = new Twitter({
 
 async function tweet() {
   try {
-    await client.post('statuses/update', { status: 'Hello world!' });
+    const searchResults = await youtube.search.list({
+      part: ['snippet'],
+      q: 'dogs',
+      maxResults: 50,
+      relevanceLanguage: 'en',
+      type: 'video',
+      safeSearch: 'strict',
+    });
+    const results = searchResults.data?.items;
+    const chosenResult = results[Math.floor(Math.random() * results.length)];
+    const { title } = chosenResult.snippet || {};
+    if (title == null) return;
+    await client.post('statuses/update', { status: `${title} [10 hours]` });
   } catch (err) {
     console.log(err);
   }
